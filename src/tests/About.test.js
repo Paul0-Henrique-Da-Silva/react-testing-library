e7e67178cc8f7874e3d9647daf73acc59ce1bd90
Paul0-Henrique-Da-Silva <ph46163835@gmail.com>
2022-06-07 15:45:01 -0300

    iniciando projeto

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,9 @@
-test('', () => {});
+const { render, screen } = require("@testing-library/react");
+import About from '../pages/About'
+
+test('Teste o componente <About.js />', () => {
+    render(<About />)
+    const pokeTitleEl = screen.getByRole('heading', { name: /About Pokédex/i , level:2 } );
+    
+    expect(pokeTitleEl).toBeInTheDocument();
+});
