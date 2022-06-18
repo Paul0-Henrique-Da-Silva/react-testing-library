a8ad1a629a9a9bc4f908be9644ee962d9a237bd1
Paul0-Henrique-Da-Silva <ph46163835@gmail.com>
2022-06-18 16:23:47 -0300

    exer 3 o,ok

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,10 +1,30 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
 import FavoritePokemons from '../pages/FavoritePokemons';
+import App from '../App';
 
+function renderWithRouter(
+  ui,
+  { route = '/', history = createMemoryHistory({ initialEntries: [route] }) } = {},
+) {
+  return {
+    ...render(<Router history={ history }>{ui}</Router>),
+    history,
+  };
+}
 describe('Teste o componente <FavoritePokemons.js />', () => {
   test('Teste se é exibida na tela a mensagem `No favorite pokemon found`', () => {
     render(<FavoritePokemons />);
     expect(screen.getByText('No favorite pokemon found')).toBeInTheDocument();
   });
+});
+
+test('Teste se são exibidos todos os cards de pokémons favoritados', () => {
+  renderWithRouter(<App />);
+  fireEvent.click(screen.getByRole('link', { name: /more details/i }));
+  fireEvent.click(screen.getByLabelText(/pokémon favoritado/i));
+  fireEvent.click(screen.getByRole('link', { name: /favorite pokémons/i }));
+  expect(screen.getByText(/average weight/i)).toBeInTheDocument();
 });
