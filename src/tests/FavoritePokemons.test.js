ccdda5b7285f57cefbf50634063aee88f8c1d828
Paul0-Henrique-Da-Silva <ph46163835@gmail.com>
2022-06-18 15:47:29 -0300

    exer 3..

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,10 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import FavoritePokemons from '../pages/FavoritePokemons';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  test('Teste se é exibida na tela a mensagem `No favorite pokemon found`', () => {
+    render(<FavoritePokemons />);
+    expect(screen.getByText('No favorite pokemon found')).toBeInTheDocument();
+  });
+});
