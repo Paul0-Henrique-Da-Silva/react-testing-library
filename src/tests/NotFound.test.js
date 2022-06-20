545a81fc68e42a17604868c2a55b8fb9c1364ff8
Paul0-Henrique-Da-Silva <ph46163835@gmail.com>
2022-06-20 18:33:01 -0300

    exer 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { screen, render } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+
+function renderWithRouter(
+  ui,
+  { route = '/', history = createMemoryHistory({ initialEntries: [route] }) } = {},
+) {
+  return {
+    ...render(<Router history={ history }>{ui}</Router>),
+    history,
+  };
+}
+
+describe('Teste o componente `<NotFound.js />', () => {
+  renderWithRouter(<NotFound />);
+  test(' Teste se a página contém `h2` com o texto `Page requested not found ', () => {
+    expect(screen.getByRole('heading', { name: /Page requested not found/i, level: 2 }))
+      .toBeInTheDocument();
+  });
+  test('Teste se a página mostra a imagem', () => {
+    renderWithRouter(<NotFound />);
+    expect(screen.getByAltText(/Pikachu crying because the page /i).src)
+      .toEqual('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
